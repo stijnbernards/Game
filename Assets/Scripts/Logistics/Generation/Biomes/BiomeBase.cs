6c5496236d20f857c3691d5202af987088ca7739
stijnhero <admin@heroicgamers.com>
2015-07-03 06:07:17 +0200

    Main changes

---
+++ Assets/Scripts/Logistics/Generation/Biomes/BiomeBase.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BiomeBase : MonoBehaviour {
+
+    public GameObject groundSprite;
+    public List<GameObject> decals;
+    public int height;
+    public int width;
+
+    private Vector2 SpawnPoint;
+
+    public void Start()
+    {
+        SpawnPoint = transform.position;
+
+        this.BuildLandPlot();
+        StartCoroutine(RenderRoad());
+    }
+
+    public virtual void BuildLandPlot()
+    {
+        for(int i = 0; i < height; i++)
+            for(int i2 = 0; i2 < width; i2++)
+                SpawnBlock(new Vector2(i, i2));
+    }
+
+    public virtual IEnumerator RenderDecal()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+
+        }
+    }
+
+    public virtual IEnumerator RenderRoad()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    public bool SpawnBlock(Vector2 OffsetLocation)
+    {
+        GameObject groundTile = GameObject.Instantiate(groundSprite, OffsetLocation, Quaternion.identity) as GameObject;
+        groundTile.transform.position = OffsetLocation;
+        groundTile.transform.parent = transform.FindChild("Ground");
+        return true;
+    }
+}
